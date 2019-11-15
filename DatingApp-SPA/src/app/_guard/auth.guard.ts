25938ccb939c2548bc9b3b06a4b224941b78d1e0
Pankaj Shyani <pshyani@yahoo.com>
2019-11-14 22:41:53 -0500

    Added Routes

---
+++ DatingApp-SPA/src/app/_guard/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Route, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, 
+    private router: Router,
+    private alertify : AlertifyService) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+   
+      if(this.authService.loggedIn())
+        return true;
+
+      this.alertify.error('You can not pass it without login');
+      this.router.navigate(['/home']);
+      return false;
+  }
+  
+}
